76544dcd502b96aefa56b808685e72e215ed2c3b
アニップ <Hanivan@users.noreply.github.com>
2024-02-17 22:31:40 +0700

    chore: use persist

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+
+import { AuthStore } from "@/features/store/authStore";
+
 const AdminDashboardPage = () => {
+  const [email, hashId, token] = AuthStore((s) => [
+    s.email,
+    s.hashId,
+    s?.token,
+  ]);
+
+  console.log(email, hashId, token);
+
   return (
     <>
       <div>AdminDashboardPage</div>
+      <p>oke</p>
+      <h1>{email}</h1>
+      <h2>{hashId}</h2>
+      <h3>{token}</h3>
     </>
   );
 };
--- src/features/store/authStore.ts
+++ src/features/store/authStore.ts
@@ -1,7 +1,7 @@
-import { create } from "zustand";
-
 import axios from "axios";
 import { randomBytes } from "crypto";
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 interface AuthStore {
   email: string | null;
@@ -21,99 +21,115 @@ interface AuthStore {
  * permisi kak. ada yang tau tidak bagaimana menyimpan email dan hashidnya keluar? saya menggunakan zustand
  */
 
-export const AuthStore = create<AuthStore>((set, get) => ({
-  email: "",
-  hashId: "",
-  token: null,
-  register: async (email, password) => {
-    try {
-      const response = {
-        status: 200,
-        data: {
-          token: randomBytes(Math.ceil(32 / 2))
-            .toString("hex")
-            .slice(0, 32),
-        },
-      };
-      // await axios.post(
-      //   `${process.env.DEV_LOCAL}/api/v1/login-or-register`,
-      //   {
-      //     email,
-      //     open_id: "email",
-      //     password,
-      //   }
-      // );
-      if (response.status === 200) {
-        set({ token: response.data.token, email, hashId: response.data.token });
-        sessionStorage.setItem("token", response.data.token);
-      } else {
-        console.log(response);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  },
-  login: async (email, password) => {
-    console.log(
-      {
-        email: get().email,
-        hashId: get().hashId,
+export const AuthStore = create(
+  persist<AuthStore>(
+    (set, get) => ({
+      email: "",
+      hashId: "",
+      token: null,
+      register: async (email, password) => {
+        try {
+          const response = {
+            status: 200,
+            data: {
+              token: randomBytes(Math.ceil(32 / 2))
+                .toString("hex")
+                .slice(0, 32),
+            },
+          };
+          // await axios.post(
+          //   `${process.env.DEV_LOCAL}/api/v1/login-or-register`,
+          //   {
+          //     email,
+          //     open_id: "email",
+          //     password,
+          //   }
+          // );
+          if (response.status === 200) {
+            set({
+              token: response.data.token,
+              email,
+              hashId: response.data.token,
+            });
+            sessionStorage.setItem("token", response.data.token);
+          } else {
+            console.log(response);
+          }
+        } catch (error) {
+          console.log(error);
+        }
       },
-      "==== before"
-    );
-    try {
-      const response = {
-        status: 200,
-        data: {
-          token: randomBytes(Math.ceil(32 / 2))
-            .toString("hex")
-            .slice(0, 32),
-        },
-      };
-      if (response.status === 200) {
-        set({ token: response.data.token, email, hashId: response.data.token });
-        sessionStorage.setItem("token", response.data.token);
-      } else {
-        console.log(response);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  },
-  forgotPassword: async (email) => {
-    try {
-      const response = await axios.post(
-        `${process.env.DEV_LOCAL}/api/v1/forgot-password`,
-        {
-          email,
+      login: async (email, password) => {
+        console.log(
+          {
+            email: get().email,
+            hashId: get().hashId,
+          },
+          "==== before"
+        );
+        try {
+          const response = {
+            status: 200,
+            data: {
+              token: randomBytes(Math.ceil(32 / 2))
+                .toString("hex")
+                .slice(0, 32),
+            },
+          };
+          if (response.status === 200) {
+            set({
+              token: response.data.token,
+              email,
+              hashId: response.data.token,
+            });
+            sessionStorage.setItem("token", response.data.token);
+          } else {
+            console.log(response);
+          }
+        } catch (error) {
+          console.log(error);
         }
-      );
-      if (response?.status === 200) {
-        const res = await response?.data?.data;
-        return res;
-      }
-    } catch (error) {
-      console.log(error);
-    }
-    return undefined;
-  },
-  resetPassword: async (email, new_password, hash_id) => {
-    try {
-      const response = await axios.post(
-        `${process.env.DEV_LOCAL}/api/v1/reset-password`,
-        {
-          email,
-          new_password,
-          hash_id,
+      },
+      forgotPassword: async (email) => {
+        try {
+          const response = await axios.post(
+            `${process.env.DEV_LOCAL}/api/v1/forgot-password`,
+            {
+              email,
+            }
+          );
+          if (response?.status === 200) {
+            const res = await response?.data?.data;
+            return res;
+          }
+        } catch (error) {
+          console.log(error);
         }
-      );
-      if (response?.status === 200) {
-        const res = await response?.data;
-        return res;
-      }
-    } catch (error) {
-      console.log(error);
+        return undefined;
+      },
+      resetPassword: async (email, new_password, hash_id) => {
+        try {
+          const response = await axios.post(
+            `${process.env.DEV_LOCAL}/api/v1/reset-password`,
+            {
+              email,
+              new_password,
+              hash_id,
+            }
+          );
+          if (response?.status === 200) {
+            const res = await response?.data;
+            return res;
+          }
+        } catch (error) {
+          console.log(error);
+        }
+        return undefined;
+      },
+    }),
+    {
+      name: "auth-storage", // name of the item in the storage (must be unique)
+      storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
     }
-    return undefined;
-  },
-}));
+  )
+);
